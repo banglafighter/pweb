953175d1acd8492da91e92436b6362604e037901
hmtmcse <hmtm.cse@gmail.com>
2023-08-31 10:35:27 +0600

    Working on Rest CRUD

--- pweb/__init__.py
+++ pweb/__init__.py
@@ -9,6 +9,7 @@ from pweb.system12.pweb_interfaces import PWebComponentRegister
 from pweb_orm import pweb_orm, PWebSaaS, PWebSaaSTenantResolver, PWebBaseModel, PWebRelationalModel, PwebModel
 from pweb_orm import PWebABCModel
 from pweb_form_rest import APIBase, FileField, EnumField, CustomNestedField, pweb_endpoint, pweb_upload_endpoint, pweb_paginate_endpoint
+from pweb_form_rest import RESTDataCRUD
 
 # Reexport External Packages
 from marshmallow import fields
