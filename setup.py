616df8ecd0273b568a2a092537586f2203ec0b02
hmtmcse <hmtm.cse@gmail.com>
2023-11-20 07:27:56 +0600

    Beta Release Version

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ def get_dependencies():
 
 setup(
     name='pwebb',
-    version='0.0.7',
+    version='0.0.8',
     url='https://github.com/banglafighter/pweb',
     license='Apache 2.0',
     author='Bangla Fighter',
