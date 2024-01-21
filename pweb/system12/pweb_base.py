1a696a1e929bab83bc41178cfd081077fed9f7f8
Touhid Mia <touhid@banglafighter.com>
2024-01-21 20:22:30 +0600

    Added Status for app loading

--- pweb/system12/pweb_base.py
+++ pweb/system12/pweb_base.py
@@ -1,5 +1,10 @@
+import os
 from flask import Flask
 
 
 class PWebBase(Flask):
-    pass
+
+    def is_app_loaded(self):
+        if not self.debug or os.environ.get("WERKZEUG_RUN_MAIN") == "true":
+            return True
+        return False
